0da37aeac68edc02b3b62021f2038f34219abd12
cbonar <cbonar@d1154f06-58f1-0310-80c4-ff3d798d75a4>
2007-03-31 15:25:54 +0000

    # some cleaning + more doxygen configuration

--- trunk/tools/gendoc.bat
+++ trunk/tools/gendoc.bat
@@ -1,10 +1,10 @@
 @echo on
 
-set REGEXPRESSO_DOXY_INPUT=regexpressodoxytmp
+set REGEXPRESSO_DOXY_INPUT=..\doc\doxygen\src
 mkdir %REGEXPRESSO_DOXY_INPUT%
 
-for %%f in (RegexWorker.js regexpresso.js) do perl js2doxy.pl ..\lib\%%f > %REGEXPRESSO_DOXY_INPUT%\%%f
-doxygen .doxygen
+for %%f in (RegexWorker.js regexpresso.js) do perl js2doxy.pl ..\src\%%f > %REGEXPRESSO_DOXY_INPUT%\%%f
+doxygen doxygen.cfg
 
 pause
 rmdir /s /q %REGEXPRESSO_DOXY_INPUT%
