2854d533b4c769dec286695d8bbe2897eacf0501
cbonar <cbonar@d1154f06-58f1-0310-80c4-ff3d798d75a4>
2007-11-30 17:28:39 +0000

    # directories reorganisation

--- trunk/tools/gendoc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-REGEXPRESSO_DOXY_INPUT=../doc/doxygen/src
-rm -rf $REGEXPRESSO_DOXY_INPUT
-mkdir -p -v $REGEXPRESSO_DOXY_INPUT
-
-for f in `cd ../src; find -iwholename "*.js"`; do perl js2doxy.pl ../src/$f > $REGEXPRESSO_DOXY_INPUT/$f; done
-doxygen doxygen.cfg
