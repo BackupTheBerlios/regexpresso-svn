d1477c48338f8cacf298394087e203cdca43d850
cbonar <cbonar@d1154f06-58f1-0310-80c4-ff3d798d75a4>
2007-12-07 00:11:25 +0000

    # utility functions now extend RegExp and String classes + using $defined(...) (more compatible ?)

--- trunk/src/regexpresso.js
+++ trunk/src/regexpresso.js
@@ -4,11 +4,11 @@
 /// Classes and functions specific to the RegexPresso project.
 ///
 /// @requires mootools
-if ( $$ == null ) throw new Error("'$$' function is missing. Make sure the Mootools library has been correctly loaded.");
+if ( MooTools == null || MooTools['version'] < 1.1 ) throw new Error("'MooTools 1.1+' is missing. Make sure the Mootools library has been correctly loaded.");
 /// @requires nicommons.ie
-if ( MSInternetExplorer == null ) throw new Error("Class 'MSInternetExplorer' is missing. Make sure the corresponding library has been correctly loaded.");
+if ( !$defined(MSInternetExplorer) ) throw new Error("Class 'MSInternetExplorer' is missing. Make sure the corresponding library has been correctly loaded.");
 /// @requires Matcher
-if ( Matcher == null ) throw new Error("Class 'Matcher' is missing. Make sure that the corresponding library has been correctly loaded.");
+if ( !$defined(Matcher) ) throw new Error("Class 'Matcher' is missing. Make sure that the corresponding library has been correctly loaded.");
 ///
 /// Placed in public domain by cbonar@users.berlios.de, 2005. Share and enjoy!
 ///
@@ -216,22 +216,45 @@ RegexWorker.prototype.getContextAsNode = function ( match, mode )
 	When instanciated, it initializes the application global parameters, sets default values, etc.
 	Therefore, it should be instanciated only once.
 
-	@tparam Element input_subject	The input field containing the subject to apply the regex to
-	@tparam Element input_regex	The input field containing the regular expression pattern to apply to the subject
+	TODO : moo-ishize this class (initialize, options, ...)
 */
-RegExpresso = function( output, input_subject, input_regex )
+RegExpresso = function( args )
 {
-	this.dom_output = $(output);
-	this.dom_subject = $(input_subject);
-	this.dom_regex = $(input_regex);
+	console.debug("new RegExpresso(",args,")");
 
-	// all options are gathered in this table
-	this.options = {
-		'accordion_opacity': true,
-		'tabswapper_smooth': true,
-		'autorefresh': false
+	// output div : where to print the results
+	this.dom_output = $(args['widgets']['output'][0]);
+
+	// exact input values from which to compute the results
+	this.dom_subject = $(args['widgets']['subject'][0]);
+	this.dom_subject.makeResizable({
+		handle: $(args['widgets']['subject'][1]),
+		modifiers:{x: false, y:'height'} /*limit the sizing to vertical*/
+	});
+	this.dom_regex = $(args['widgets']['regex'][0]);
+
+	// widgets to help the user to type the regular expression in input
+	this.widget_search = {
+		0: $(args['widgets']['search'][0]),
+		'i': $(args['widgets']['search'][1]),
+		'g': $(args['widgets']['search'][2]),
+		'm': $(args['widgets']['search'][3])
+	};
+	this.widget_replace = {
+		0: $(args['widgets']['replace'][0]),
+		1: $(args['widgets']['replace'][1]),
+		'i': $(args['widgets']['replace'][2]),
+		'g': $(args['widgets']['replace'][3]),
+		'm': $(args['widgets']['replace'][4])
+	};
+	this.widget_expert = {
+		0: $(args['widgets']['expert'][0])
 	};
 
+	// the div where to print the options
+	this.dom_options = $(args['widgets']['options'][0]);
+	// the default values for all options
+	this.options = args['options'];
 
 	// once of two, we alternate the style in order to show something has been done
 	// even if the result is exactly the same
@@ -293,41 +316,40 @@ RegExpresso = function( output, input_subject, input_regex )
 */
 RegExpresso.prototype.onFieldUpdate = function( el )
 {
-	var input_regex = $('input_regex');
-	var oldval = input_regex.value;
+	var oldval = this.dom_regex.value;
 
 	// updates the regex with the value of the current tab
 	switch( $(el).id )
 	{
-		case 'regex_search_a':
-		case 'regex_search_mod_i':
-		case 'regex_search_mod_g':
-		case 'regex_search_mod_m':
-			var modifiers = ( $('regex_search_mod_i').checked ? 'i' : '' ) + ( $('regex_search_mod_g').checked ? 'g' : '' ) + ($('regex_search_mod_m').checked ? 'm' : '' );
-			input_regex.value = "/" + $('regex_search_a').value + "/" + modifiers;
-			$('regex_expert_a').value = input_regex.value;	// see comment on 'regex_expert'
+		case this.widget_search[0].id:
+		case this.widget_search['i'].id:
+		case this.widget_search['g'].id:
+		case this.widget_search['m'].id:
+			var modifiers = ( this.widget_search['i'].checked ? 'i' : '' ) + ( this.widget_search['g'].checked ? 'g' : '' ) + (this.widget_search['m'].checked ? 'm' : '' );
+			this.dom_regex.value = "/" + this.widget_search[0].value + "/" + modifiers;
+			this.widget_expert[0].value = this.dom_regex.value;	// see comment on 'regex_expert'
 			break;
 
-		case 'regex_replace_a':
-		case 'regex_replace_b':
-		case 'regex_replace_mod_i':
-		case 'regex_replace_mod_g':
-		case 'regex_replace_mod_m':
-			var modifiers = ( $('regex_replace_mod_i').checked ? 'i' : '' ) + ( $('regex_replace_mod_g').checked ? 'g' : '' ) + ($('regex_replace_mod_m').checked ? 'm' : '' );
-			input_regex.value = "s/" + $('regex_replace_a').value + "/" + $('regex_replace_b').value + "/" + modifiers;
-			$('regex_expert_a').value = input_regex.value;	// see comment on 'regex_expert'
+		case this.widget_replace[0].id:
+		case this.widget_replace[1].id:
+		case this.widget_replace['i'].id:
+		case this.widget_replace['g'].id:
+		case this.widget_replace['m'].id:
+			var modifiers = ( this.widget_replace['i'].checked ? 'i' : '' ) + ( this.widget_replace['g'].checked ? 'g' : '' ) + (this.widget_replace['m'].checked ? 'm' : '' );
+			this.dom_regex.value = "s/" + this.widget_replace[0].value + "/" + this.widget_replace[1].value + "/" + modifiers;
+			this.widget_expert[0].value = this.dom_regex.value;	// see comment on 'regex_expert'
 			break;
 
 		// expert mode works differently than 'easy' modes : it reuses the regex that was set by the other modes
 		// because it's expected users will come to this mode after having tried in simple modes first
 		// This also enables to observe how to compose a regular expression in expert mode
-		case 'regex_expert_a':
-			input_regex.value = $('regex_expert_a').value;
+		case this.widget_expert[0].id:
+			this.dom_regex.value = this.widget_expert[0].value;
 			break;
 	}
 
 	// updates the result if needed
-	if ( this.options['autorefresh'] && input_regex.value != oldval )
+	if ( this.options['autorefresh'] && this.dom_regex.value != oldval )
 	{
 		this.onSubmit();
 	}
@@ -343,25 +365,25 @@ RegExpresso.prototype.onSubmit = function()
 {
 	console.debug(this,".onSubmit()");
 
-	// makes sure even the first time the pattern used is the one printed
-	// (when no change has occured yet in the fields)
+	// makes sure the pattern used will be the one in the current tab even if its content has not changed
 	console.debug("switch(",this.tabs_regex.tabs[this.tabs_regex.now],")");
 	switch ( this.tabs_regex.now )
 	{
 		case 0:
-			this.onFieldUpdate($('regex_search_a'));
-			this.onFieldUpdate($('regex_search_mod_i'));
-			this.onFieldUpdate($('regex_search_mod_g'));
-			this.onFieldUpdate($('regex_search_mod_m'));
+			this.onFieldUpdate( this.widget_search[0] );
+			this.onFieldUpdate( this.widget_search['i'] );
+			this.onFieldUpdate( this.widget_search['g'] );
+			this.onFieldUpdate( this.widget_search['m'] );
 			break;
 		case 1:
-			this.onFieldUpdate($('regex_replace_a'));
-			this.onFieldUpdate($('regex_replace_mod_i'));
-			this.onFieldUpdate($('regex_replace_mod_g'));
-			this.onFieldUpdate($('regex_replace_mod_m'));
+			this.onFieldUpdate( this.widget_replace[0] );
+			this.onFieldUpdate( this.widget_replace[1] );
+			this.onFieldUpdate( this.widget_replace['i'] );
+			this.onFieldUpdate( this.widget_replace['g'] );
+			this.onFieldUpdate( this.widget_replace['m'] );
 			break;
 		case 2:
-			this.onFieldUpdate($('regex_expert_a'));
+			this.onFieldUpdate(this.widget_expert);
 			break;
 	}
 
@@ -369,29 +391,36 @@ RegExpresso.prototype.onSubmit = function()
 	{
 		// the following operation can take time if the input text is big
 		this.worker = new RegexWorker( this.dom_subject.value, this.dom_regex.value );
-		switch ( explainPerlRegexPattern(this.worker.pattern).action )
+		switch ( RegExp.explainPerlRegexPattern(this.worker.pattern).action )
 		{
 			case 'm':
 			case '':
 			{
 				// first, prints the number of matches
-				var howmany_text = "No match.";
-				if ( this.worker.matches.length > 0 )
+				if ( this.options['show_count'] )
 				{
-					if ( this.worker.matches.length == 1 )
-						howmany_text = "1 match";
-					else
-						howmany_text = this.worker.matches.length + " matches";
+					var howmany_text = "No match.";
+					if ( this.worker.matches.length > 0 )
+					{
+						if ( this.worker.matches.length == 1 )
+							howmany_text = "1 match";
+						else
+							howmany_text = this.worker.matches.length + " matches";
+					}
+					// TODO : a special div for the count and/or a special tab with stats
+					this.dom_output.empty().appendChild( document.createSimpleElement("div",this.desc_again.next(), document.createTextNode(howmany_text)) );
+					this.dom_output.setOpacity(1);
 				}
-				// TODO : a special div for the count and/or a special tab with stats
-				while ( this.dom_output.childNodes.length > 0 )
-					this.dom_output.removeChild(this.dom_output.firstChild);
-				this.dom_output.appendChild( document.createSimpleElement("div",this.desc_again.next(), document.createTextNode(howmany_text)) );
+				else
+				{
+					// hides this section if required
+					this.dom_output.setOpacity(0);
+				}
 
 				// then outputs the different kind of available representations
 				// the following operations can take time if the input text is big
 				// TODO : implement addTab in the following code
-				/*if ( this.worker.matches.length > 0 )
+				/*if ( this.options['show_results'] && this.worker.matches.length > 0 )
 				{
 					this.dom_output.addTab( "output_text", this.worker.asHTML() );
 					this.dom_output.addTab( "output_table", this.worker.asTable() );
@@ -402,15 +431,25 @@ RegExpresso.prototype.onSubmit = function()
 			case 's':
 			{
 				// first, prints the number of matches
-				var howmany_text = this.worker.matches.length > 0 ? this.worker.matches.length + " replaced :" : "Nothing was replaced.";
-				while ( this.dom_output.childNodes.length > 0 )
-					this.dom_output.removeChild(this.dom_output.firstChild);
-				this.dom_output.appendChild( document.createSimpleElement("div",this.desc_again.next(), document.createTextNode(howmany_text)) );
+				if ( this.options['show_count'] )
+				{
+					var howmany_text = this.worker.matches.length > 0 ? this.worker.matches.length + " replaced :" : "Nothing was replaced.";
+					this.dom_output.empty().appendChild( document.createSimpleElement("div",this.desc_again.next(), document.createTextNode(howmany_text)) );
+					this.dom_output.setOpacity(1);
+				}
+				else
+				{
+					// hides this section if required
+					this.dom_output.setOpacity(0);
+				}
 
 				// then outputs the different kind of available representations
 				// the following operations can take time if the input text is big
-				/*this.dom_output.addTab( "output_text", this.worker.asHTML() );
-				this.dom_output.addTab( "output_text", this.worker.asText() );*/
+				/*if ( this.options['show_results'] )
+				{
+				this.dom_output.addTab( "output_text", this.worker.asHTML() );
+				this.dom_output.addTab( "output_text", this.worker.asText() );
+				}*/
 			}
 			break;
 		}
